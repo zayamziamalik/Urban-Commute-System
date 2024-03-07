5a873ee84d096f5ce37e7da1695cb8e184153917
Zayam Zia Malik <61197095+zayamziamalik@users.noreply.github.com>
2024-03-07 18:32:02 +0500

    Delete src directory

--- src/main/java/com/mycompany/repo/BusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.repo;
-
-import com.mycompany.models.Bus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BusRepository extends JpaRepository<Bus,Integer> {
-}
